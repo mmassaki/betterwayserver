3259dd9776048ed62eb530b2df50ac0a768303f7
marcio.massaki <marcio.massaki@78db619c-a216-bc24-8f4a-c99eea380188>
2010-11-06 21:00:59 +0000

    Criacao do projeto

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+BetterWay::Application.initialize!
+
+# Rails::Initializer.run do |config|
+# config.plugins = [:all]
+# config.i18n.default_locale = “pt-BR”
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BetterWay::Application.config.secret_token = 'eae060e8cfe2f15dcc628da5ea688910a92e86e48cd1ca9da244b2b2fcffc4a804ddb98a18b6ceb72a9dddf2f5b8068d95cc7b31a0f1550f4be5f1403a8c44a5'
